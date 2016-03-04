dc4041750a48f4b6041d3575036d30316a57fe33
Taier <oneander@gmail.com>
2016-03-04 07:35:59 +0200

    ADD RAW manual controls

--- APhoto/APhoto/CameraController.swift
+++ APhoto/APhoto/CameraController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
-import MobileCoreServices // for kUTTypeImage
+import MobileCoreServices // for kUTTypeImage // Remove
+import AVFoundation
 
 class CameraController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     // Variables
-    var imagePicker: UIImagePickerController!
+    var imagePicker: UIImagePickerController! //Remove
     var takePhoto:Bool = false
     
+    let captureSession = AVCaptureSession()
+    var captureDevice : AVCaptureDevice?
+    
+    @IBOutlet weak var viewCamera: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        let devices = AVCaptureDevice.devices()
+        print(devices)
+        
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -32,19 +42,100 @@ class CameraController: UIViewController, UIImagePickerControllerDelegate, UINav
     }
 
     func lauchCamera() {
-        if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerControllerSourceType.Camera) {
-                imagePicker = UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.sourceType =
-                    UIImagePickerControllerSourceType.Camera
-                imagePicker.mediaTypes = [kUTTypeImage as String]
-                imagePicker.allowsEditing = false
-                self.presentViewController(imagePicker, animated: false,
-                    completion: nil)
-        } else {
-            print("Can't use camera");
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        let devices = AVCaptureDevice.devices()
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                }
+            }
         }
+        
+        if captureDevice != nil {
+            beginSession()
+        }
+    }
+    
+    // MAR: Camera stuff
+    
+    func beginSession() {
+        
+        configureDevice()
+        
+        try! captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer)
+        previewLayer?.frame = self.viewCamera.layer.frame
+        captureSession.startRunning()
+    }
+    
+    func configureDevice() {
+        if let device = captureDevice {
+            try! device.lockForConfiguration()
+            device.focusMode = .Locked
+            device.unlockForConfiguration()
+        }
+    }
+    
+    func updateDeviceSettings(focusValue : Float, isoValue : Float) {
+        if let device = captureDevice {
+            
+            do {
+                try device.lockForConfiguration()
+            } catch {
+                return
+            }
+                device.setFocusModeLockedWithLensPosition(focusValue, completionHandler: { (time) -> Void in
+                    //
+                })
+                
+                // Adjust the iso to clamp between minIso and maxIso based on the active format
+                let minISO = device.activeFormat.minISO
+                let maxISO = device.activeFormat.maxISO
+                let clampedISO = isoValue * (maxISO - minISO) + minISO
+                
+                device.setExposureModeCustomWithDuration(AVCaptureExposureDurationCurrent, ISO: clampedISO, completionHandler: { (time) -> Void in
+                    //
+                })
+                
+                device.unlockForConfiguration()
+        }
+    }
+    
+    let screenWidth = UIScreen.mainScreen().bounds.size.width
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch =  touches.first{
+            let touchPer = touchPercent(touch)
+            updateDeviceSettings(Float(touchPer.x), isoValue: 0.5)
+        }
+      
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch =  touches.first{
+            let touchPer = touchPercent(touch)
+            updateDeviceSettings(Float(touchPer.x), isoValue: 1)
+        }
+    }
+    
+    func touchPercent(touch : UITouch) -> CGPoint {
+        // Get the dimensions of the screen in points
+        let screenSize = UIScreen.mainScreen().bounds.size
+        
+        // Create an empty CGPoint object set to 0, 0
+        var touchPer = CGPointZero
+        
+        // Set the x and y values to be the value of the tapped position, divided by the width/height of the screen
+        touchPer.x = touch.locationInView(self.view).x / screenSize.width
+        touchPer.y = touch.locationInView(self.view).y / screenSize.height
+        
+        // Return the populated CGPoint
+        return touchPer
     }
     
     // MARK: Navigation

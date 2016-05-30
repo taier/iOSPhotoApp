be0b082e02688fa0abf7a2c1f2b6aa5fb8d0d20b
Taier <oneander@gmail.com>
2016-05-30 08:24:44 +0300

    Apple Watch support

---
+++ APhoto/APhotoWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  APhotoWatch Extension
+//
+//  Created by Deniss Kaibagarovs on 27/05/16.
+//  Copyright © 2016 Sudo Mobi. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
